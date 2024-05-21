aaae63ff55442b545c4012a213c86b94c0837210
thiersfb <thiers.fb@gmail.com>
2024-05-21 18:19:25 -0300

    DAO login de controle de acesso

---
+++ spring_jsf_primefaces/src/main/java/br/com/dao/implementacao/DaoLogin.java
@@ -0,0 +1,22 @@
+package br.com.dao.implementacao;
+
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.stereotype.Repository;
+
+import br.com.framework.implementacao.crud.ImplementacaoCrud;
+import br.com.repository.interfaces.RepositoryLogin;
+
+@Repository
+public class DaoLogin extends ImplementacaoCrud<Object> implements RepositoryLogin {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public boolean autentico(String login, String senha) throws Exception {
+		
+		String sql = "SELECT COUNT(1) AS Autentica FROM Entidade WHERE ent_login = ? AND ent_senha = ? ";
+		SqlRowSet sqlRowSet = super.getJdbcTemplate().queryForRowSet(sql, new Object[]{login, senha});
+		return false;
+	}
+
+}
